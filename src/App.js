86cecdf8f426186033716278f89b3e1744772a73
Malie Kapelianis <maliekapelianis@gmail.com>
2024-08-08 14:37:06 +0200

    Created Weather component with placeholder information

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <p>
-        This project was coded by{" "}
-        <a
-          href="https://maliekapdev-portfolio.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Malie Kapelianis
-        </a>{" "}
-        and is open-sourced on{" "}
-        <a
-          href="https://github.com/MalieKapDev/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-      </p>
+      <div className="container">
+        <h1>Weather App</h1>
+        <Weather />
+        <p>
+          This project was coded by{" "}
+          <a
+            href="https://maliekapdev-portfolio.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Malie Kapelianis
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/MalieKapDev/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
