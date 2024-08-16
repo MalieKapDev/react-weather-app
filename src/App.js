7aeea400692bd09a93978073c53a205017f47cc7
Malie Kapelianis <maliekapelianis@gmail.com>
2024-08-16 14:57:29 +0200

    Imported background image and updated App.js and App.css

--- src/App.js
+++ src/App.js
@@ -7,33 +7,35 @@ export default function App() {
     <div className="App">
       <div className="container">
         <Weather defaultCity="Pretoria" />
-        <p>
-          This project was coded by{" "}
-          <a
-            href="https://maliekapdev-portfolio.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Malie Kapelianis
-          </a>{" "}
-          and is open-sourced on{" "}
-          <a
-            href="https://github.com/MalieKapDev/react-weather-app"
-            target="_blank"
-            rel="noreferrer"
-          >
-            GitHub
-          </a>{" "}
-          and hosted on{" "}
-          <a
-            href="https://react-weather-app-maliekapdev.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Netlify
-          </a>
-          .
-        </p>
+        <footer>
+          <p>
+            This project was coded by{" "}
+            <a
+              href="https://maliekapdev-portfolio.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Malie Kapelianis
+            </a>{" "}
+            and is open-sourced on{" "}
+            <a
+              href="https://github.com/MalieKapDev/react-weather-app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              GitHub
+            </a>{" "}
+            and hosted on{" "}
+            <a
+              href="https://react-weather-app-maliekapdev.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Netlify
+            </a>
+            .
+          </p>
+        </footer>
       </div>
     </div>
   );
